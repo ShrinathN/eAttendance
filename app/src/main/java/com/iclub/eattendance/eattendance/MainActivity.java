4f9ae1121482b3797b560812ade7572135d96b90
Shrinath Nimare <shrinath.nimare@gmail.com>
2018-07-16 21:03:09 +0530

    Fixes #1

--- app/src/main/java/com/iclub/eattendance/eattendance/MainActivity.java
+++ app/src/main/java/com/iclub/eattendance/eattendance/MainActivity.java
@@ -1,54 +1,53 @@
 package com.iclub.eattendance.eattendance;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
     // to be used as macros
     final byte BARCODE_SCAN = 1;
     final byte QRCODE_SCAN = 2;
 
     //all the variables to be used
-    //related to ui updating
-    public boolean uiUpdateRequest = false;
-    public byte uiUpdateRequestType = 0;
     public String barcode = null;
     public String qrcode = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if(savedInstanceState != null) //meaning the activity was redrawn, so saved data from the bundle must be retrieved
+        {
+            barcode = (String)savedInstanceState.getSerializable("barcode"); //get the barcode string from the saved bundle
+            qrcode = (String)savedInstanceState.getSerializable("qrcode");//get the qrcode string from the saved bundle
+        }
+
         //just some UI elements being declared
         final Button button_scanBarcode = (Button)findViewById(R.id.button_scanBarcode);
         final Button button_scanQr = (Button)findViewById(R.id.button_scanQr);
         final Button button_next = (Button)findViewById(R.id.button_next);
 
+        //this handler-runnable combo will update the UI elements
         final Handler handlerToUpdateUi = new Handler(); //this handler will update the UI elements
         Runnable runnableToUpdateUi = new Runnable() {
             @Override
             public void run() {
-                if(uiUpdateRequest) // run only if the UI update request variable is true
-                {
-                    uiUpdateRequest = false; //set the variable as false, ie reset it
-                    if(uiUpdateRequestType == BARCODE_SCAN)
-                    {
-                        button_scanBarcode.setText("Rescan\n" + barcode);
-                    }
-                    else if(uiUpdateRequestType == QRCODE_SCAN)
-                    {
-                        button_scanQr.setText("Rescan\n" + qrcode);
-                    }
-                    if((barcode != null) && (qrcode != null)) //if both strings are not empty, hence meaning both details have been scanned, proceed to make the next button visible
-                        button_next.setVisibility(View.VISIBLE);
-                }
-                handlerToUpdateUi.postDelayed(this,300);
+                if (barcode != null) //if the barcode has already been scanned, the default value of string barcode is null
+                    button_scanBarcode.setText("Rescan\n" + barcode); //set the button's text as
+                if (qrcode != null) //if the qrcode has already been scanned, the default value of string qrcode is null
+                    button_scanQr.setText("Rescan\n" + qrcode); //set the button's text as
+
+                if ((barcode != null) && (qrcode != null)) //if both strings are not empty, hence meaning both details have been scanned, proceed to make the next button visible
+                    button_next.setVisibility(View.VISIBLE); //makes the next activity button visible
+                handlerToUpdateUi.postDelayed(this, 300); //recursion to loop the runnable indefinitely
             }
         };
         handlerToUpdateUi.postDelayed(runnableToUpdateUi, 0);
@@ -62,7 +61,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //this is the onclick listener for QRcode
+        //this is the onclick listener for QRcode s
         button_scanQr.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -72,21 +71,25 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    //when the result of scanning activity is
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent resultIntent)
     {
-        uiUpdateRequest = true;
-        if(requestCode == BARCODE_SCAN)
-        {
-            uiUpdateRequestType = BARCODE_SCAN;
-            barcode = resultIntent.getStringExtra("SCAN_RESULT");
-        }
-        else if(requestCode == QRCODE_SCAN)
-        {
-            uiUpdateRequestType = QRCODE_SCAN;
-            qrcode = resultIntent.getStringExtra("SCAN_RESULT");
+        if(resultCode != RESULT_CANCELED) { //only execute if the barcode scanning activity was not closed, and was successful
+            if (requestCode == BARCODE_SCAN) { //if barcode was scanned
+                barcode = resultIntent.getStringExtra("SCAN_RESULT"); //set string barcode as scanned string
+            } else if (requestCode == QRCODE_SCAN) { //else if qrcode was scanned
+                qrcode = resultIntent.getStringExtra("SCAN_RESULT"); //set string qrcode as scanned string
+            }
         }
     }
 
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        //saves activity data for resuming when the activity is redrawn (for example after an orientation change)
+        outState.putSerializable("barcode", barcode);
+        outState.putSerializable("qrcode", qrcode);
+    }
 
 }
