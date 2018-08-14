5dc9d1b14e75cfe81257e67f3f9cfbb86f88e092
Shrinath Nimare <shrinath.nimare@gmail.com>
2018-08-14 14:31:52 +0530

    Optimized app for lower CPU usage. Some misc changes

--- app/src/main/java/com/iclub/eattendance/eattendance/MainActivity.java
+++ app/src/main/java/com/iclub/eattendance/eattendance/MainActivity.java
@@ -8,10 +8,12 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+
 import javax.net.ssl.HttpsURLConnection;
 
 
@@ -32,6 +34,31 @@ public class MainActivity extends AppCompatActivity {
     public String qrcode = null;
     public String toastString = null; //used to make toasts from inside a thread
 
+    //just some UI elements being declared
+    Button button_scanBarcode;
+    Button button_scanQr;
+    Button button_next;
+
+    //this handler-runnable combo will update the UI elements (on call only)
+    final Handler handlerToUpdateUi = new Handler(); //this handler will update the UI elements
+    Runnable runnableToUpdateUi = new Runnable() {
+        @Override
+        public void run() {
+            if (barcode != null) //if the barcode has already been scanned, the default value of string barcode is null
+                button_scanBarcode.setText("Rescan\n" + barcode); //set the button's text as
+            if (qrcode != null) //if the qrcode has already been scanned, the default value of string qrcode is null
+                button_scanQr.setText("Rescan\n" + qrcode); //set the button's text as
+
+            if ((barcode != null) && (qrcode != null)) //if both strings are not empty, hence meaning both details have been scanned, proceed to make the next button visible
+                button_next.setVisibility(View.VISIBLE); //makes the next activity button visible
+
+            if (toastString != null) {
+                Toast.makeText(MainActivity.this, toastString, Toast.LENGTH_LONG).show();
+                toastString = null;
+            }
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,34 +68,14 @@ public class MainActivity extends AppCompatActivity {
         {
             barcode = (String) savedInstanceState.getSerializable("barcode"); //get the barcode string from the saved bundle
             qrcode = (String) savedInstanceState.getSerializable("qrcode");//get the qrcode string from the saved bundle
+            handlerToUpdateUi.postDelayed(runnableToUpdateUi, 0); //redraw UI
         }
 
-        //just some UI elements being declared
-        final Button button_scanBarcode = (Button) findViewById(R.id.button_scanBarcode);
-        final Button button_scanQr = (Button) findViewById(R.id.button_scanQr);
-        final Button button_next = (Button) findViewById(R.id.button_next);
 
-        //this handler-runnable combo will update the UI elements
-        final Handler handlerToUpdateUi = new Handler(); //this handler will update the UI elements
-        Runnable runnableToUpdateUi = new Runnable() {
-            @Override
-            public void run() {
-                if (barcode != null) //if the barcode has already been scanned, the default value of string barcode is null
-                    button_scanBarcode.setText("Rescan\n" + barcode); //set the button's text as
-                if (qrcode != null) //if the qrcode has already been scanned, the default value of string qrcode is null
-                    button_scanQr.setText("Rescan\n" + qrcode); //set the button's text as
+        button_scanBarcode = (Button) findViewById(R.id.button_scanBarcode);
+        button_scanQr = (Button) findViewById(R.id.button_scanQr);
+        button_next = (Button) findViewById(R.id.button_next);
 
-                if ((barcode != null) && (qrcode != null)) //if both strings are not empty, hence meaning both details have been scanned, proceed to make the next button visible
-                    button_next.setVisibility(View.VISIBLE); //makes the next activity button visible
-
-                if (toastString != null) {
-                    Toast.makeText(MainActivity.this, toastString, Toast.LENGTH_LONG).show();
-                    toastString = null;
-                }
-                handlerToUpdateUi.postDelayed(this, 300); //recursion to loop the runnable indefinitely
-            }
-        };
-        handlerToUpdateUi.postDelayed(runnableToUpdateUi, 0);
 
         //this is the onclick listener for the barcode to identify the user
         button_scanBarcode.setOnClickListener(new View.OnClickListener() {
@@ -76,6 +83,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intentToScanBarcode = new Intent("com.google.zxing.client.android.SCAN");
                 startActivityForResult(intentToScanBarcode, BARCODE_SCAN);
+
             }
         });
 
@@ -114,6 +122,8 @@ public class MainActivity extends AppCompatActivity {
                             } else {
                                 Intent intentToStartAttendanceActivity = new Intent(MainActivity.this, attendance_activity.class);
                                 intentToStartAttendanceActivity.putExtra("responseFromTheServer", responseFromTheServer);
+                                intentToStartAttendanceActivity.putExtra("barcode",barcode);
+                                intentToStartAttendanceActivity.putExtra("qrcode",qrcode);
                                 startActivity(intentToStartAttendanceActivity);
                             }
                         } catch (Exception e) {
@@ -145,6 +155,7 @@ public class MainActivity extends AppCompatActivity {
             } else if (requestCode == QRCODE_SCAN) { //else if qrcode was scanned
                 qrcode = resultIntent.getStringExtra("SCAN_RESULT"); //set string qrcode as scanned string
             }
+            handlerToUpdateUi.postDelayed(runnableToUpdateUi, 0);
         }
     }
 
--- app/src/main/java/com/iclub/eattendance/eattendance/attendance_activity.java
+++ app/src/main/java/com/iclub/eattendance/eattendance/attendance_activity.java
@@ -10,16 +10,8 @@ import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import org.json.JSONArray;
 import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
-import javax.net.ssl.HttpsURLConnection;
 
 public class attendance_activity extends AppCompatActivity {
 
@@ -29,6 +21,14 @@ public class attendance_activity extends AppCompatActivity {
     final String CLASS_ATTENDANCE_SUBMISSION_URL_SSL = "https://192.168.0.110/www/attendance.php";
     final String CLASS_ATTENDANCE_SUBMISSION_URL = "http://192.168.0.110/www/attendance.php";
 
+    //defining all the UI widgets to be used
+    TextView label_studentsPresent;
+    TextView label_studentsAbsent;
+    TextView label_studentsInfo;
+    Button button_absent;
+    Button button_present;
+    Button button_back;
+    ProgressBar progressBar;
 
     //global data to be used
     public String responseFromTheServer; //the JSON response from the server
@@ -39,25 +39,81 @@ public class attendance_activity extends AppCompatActivity {
     public int totalJsonEntries = 0; //aka total number of students in the class
     public boolean studentAttendanceArray[] = new boolean[100]; //attendance for 100 students in a class at most
     public String toastString = null;
+    public String qrcode = null;
+    public String barcode = null;
+
+    //runnable and handler to update UI widgets
+    final Handler handlerToUpdateUi = new Handler();
+    final Runnable runnableToUpdateUi = new Runnable() {
+        @Override
+        public void run() {
+            //checks if all the students in the list have been addressed already
+            if (jsonIndex == totalJsonEntries) {
+                button_absent.setVisibility(View.INVISIBLE);
+                button_present.setVisibility(View.INVISIBLE);
+                uploadAttendance();
+            } else {
+                button_absent.setVisibility(View.VISIBLE);
+                button_present.setVisibility(View.VISIBLE);
+            }
+
+            if(toastString != null)
+            {
+                Toast.makeText(attendance_activity.this, toastString, Toast.LENGTH_LONG).show();
+                toastString = null;
+            }
+
+            //calculating the number of students absent and present
+            int tempCounter;
+            presentCounter = 0;
+            absentCounter = 0;
+            for (tempCounter = 0; tempCounter < jsonIndex; tempCounter++) {
+                if (studentAttendanceArray[tempCounter]) //if the person is present
+                    presentCounter++; //increment present counter
+                else
+                    absentCounter++; //increment absent counter
+            }
+            //setting the number of students absent and present
+            label_studentsPresent.setText(Integer.toString(presentCounter) + " students present");
+            label_studentsAbsent.setText(Integer.toString(absentCounter) + " students absent");
+            progressBar.setProgress(jsonIndex); //sets the progress bar's current progress
+            try {
+                JSONObject jsonObject = jsonArray.getJSONObject(jsonIndex); //gets the current students name
+                label_studentsInfo.setText(jsonObject.getString("name") + "\n" + Integer.toString(jsonObject.getInt("regno"))); //sets the name and the regno on the label_studentsInfo textview
+            } catch (Exception e) {
+                Log.d(DEBUG_TAG, e.toString()); //debug exception
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_attendance_activity);
 
-        //defining all the UI widgets to be used
-        final TextView label_studentsPresent = (TextView) findViewById(R.id.label_studentsPresent);
-        final TextView label_studentsAbsent = (TextView) findViewById(R.id.label_studentsAbsent);
-        final TextView label_studentsInfo = (TextView) findViewById(R.id.label_studentsInfo);
-        final Button button_absent = (Button) findViewById(R.id.button_absent);
-        final Button button_present = (Button) findViewById(R.id.button_present);
-        final Button button_back = (Button) findViewById(R.id.button_back);
-        final ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        if (savedInstanceState != null) //meaning the activity was redrawn, so saved data from the bundle must be retrieved
+        {
+            studentAttendanceArray = (boolean[])savedInstanceState.getSerializable("studentAttendanceArray");
+            jsonIndex = (int)savedInstanceState.getSerializable("jsonIndex");
+            handlerToUpdateUi.postDelayed(runnableToUpdateUi, 0); //redraw UI
+        }
+
+
+        label_studentsPresent = (TextView) findViewById(R.id.label_studentsPresent);
+        label_studentsAbsent = (TextView) findViewById(R.id.label_studentsAbsent);
+        label_studentsInfo = (TextView) findViewById(R.id.label_studentsInfo);
+        button_absent = (Button) findViewById(R.id.button_absent);
+        button_present = (Button) findViewById(R.id.button_present);
+        button_back = (Button) findViewById(R.id.button_back);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
 
 
         //getting intent from previous activity
         Intent intentFromMainActivity = getIntent();
         responseFromTheServer = intentFromMainActivity.getStringExtra("responseFromTheServer"); //getting the JSON response
+        qrcode = intentFromMainActivity.getStringExtra("qrcode");
+        barcode = intentFromMainActivity.getStringExtra("barcode");
         try {
             jsonArray = new JSONArray(responseFromTheServer); //parsing the JSON
             totalJsonEntries = jsonArray.length(); //getting the number of entries in the JSON
@@ -66,64 +122,14 @@ public class attendance_activity extends AppCompatActivity {
             Log.d(DEBUG_TAG, e.toString()); //debug exception
         }
 
-
-        //runnable and handler to update UI widgets
-        final Handler handlerToUpdateUi = new Handler();
-        final Runnable runnableToUpdateUi = new Runnable() {
-            @Override
-            public void run() {
-                //checks if all the students in the list have been addressed already
-                if (jsonIndex == totalJsonEntries) {
-                    button_absent.setVisibility(View.INVISIBLE);
-                    button_present.setVisibility(View.INVISIBLE);
-                    uploadAttendance();
-                } else {
-                    button_absent.setVisibility(View.VISIBLE);
-                    button_present.setVisibility(View.VISIBLE);
-                }
-
-                if(toastString != null)
-                {
-                    Toast.makeText(attendance_activity.this, toastString, Toast.LENGTH_LONG).show();
-                    toastString = null;
-                }
-
-                //calculating the number of students absent and present
-                int tempCounter;
-                presentCounter = 0;
-                absentCounter = 0;
-                for (tempCounter = 0; tempCounter < jsonIndex; tempCounter++) {
-                    if (studentAttendanceArray[tempCounter]) //if the person is present
-                        presentCounter++; //increment present counter
-                    else
-                        absentCounter++; //increment absent counter
-                }
-                //setting the number of students absent and present
-                label_studentsPresent.setText(Integer.toString(presentCounter) + " students present");
-                label_studentsAbsent.setText(Integer.toString(absentCounter) + " students absent");
-                progressBar.setProgress(jsonIndex); //sets the progress bar's current progress
-                try {
-                    JSONObject jsonObject = jsonArray.getJSONObject(jsonIndex); //gets the current students name
-                    label_studentsInfo.setText(jsonObject.getString("name") + "\n" + Integer.toString(jsonObject.getInt("regno"))); //sets the name and the regno on the label_studentsInfo textview
-                } catch (Exception e) {
-                    Log.d(DEBUG_TAG, e.toString()); //debug exception
-                }
-
-                handlerToUpdateUi.postDelayed(this, 150); //starts the runnable using the handler, recursively loops forever
-            }
-        };
-        handlerToUpdateUi.postDelayed(runnableToUpdateUi, 0); //starts the handler with the runnable for the first time
-
-
-
-
-
+        handlerToUpdateUi.postDelayed(runnableToUpdateUi, 0); //redraw UI
         //onclick listener for absent button
         button_absent.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 studentAttendanceArray[jsonIndex] = false; //sets the person as absent
                 jsonIndex++; //increments the json index, aka moves to the next person in the JSON list
+                handlerToUpdateUi.postDelayed(runnableToUpdateUi, 0); //starts the handler with the runnable for the first time
             }
         });
 
@@ -133,6 +139,7 @@ public class attendance_activity extends AppCompatActivity {
             public void onClick(View view) {
                 studentAttendanceArray[jsonIndex] = true; //sets the person as present
                 jsonIndex++; //increments the json index, aka moves to the next person in the JSON list
+                handlerToUpdateUi.postDelayed(runnableToUpdateUi, 0); //starts the handler with the runnable for the first time
             }
         });
 
@@ -141,6 +148,7 @@ public class attendance_activity extends AppCompatActivity {
             public void onClick(View view) {
                 if (jsonIndex != 0) //only if the index is not zero
                     jsonIndex--; //decrements the json index, aka moves to the previous person in the JSON list. Meant to serve as an way to make immediate ammends
+                handlerToUpdateUi.postDelayed(runnableToUpdateUi, 0); //starts the handler with the runnable for the first time
             }
         });
     }
@@ -160,29 +168,15 @@ public class attendance_activity extends AppCompatActivity {
             else
                 stringToSend = stringToSend + "A"; //an 'A' character if the student is absent
         }
-        try {
-            if (USE_SSL) {
-                URL url = new URL(CLASS_ATTENDANCE_SUBMISSION_URL_SSL + "?attendance_list=" + stringToSend); //setting the URL to make the GET request
-                HttpsURLConnection httpsURLConnection = (HttpsURLConnection) url.openConnection();
-                httpsURLConnection.connect(); //connect to the server
-                httpsURLConnection.disconnect(); //disconnect
-            } else { //if SSL is not needed
-                URL url = new URL(CLASS_ATTENDANCE_SUBMISSION_URL + "?attendance_list=" + stringToSend); //setting the URL to make the GET request
-                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-                httpURLConnection.setRequestMethod("GET"); //setting the method of the request
-                httpURLConnection.connect(); //connect to the URL
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
-                responseFromTheServer = bufferedReader.readLine(); //receive the response from the server
-                Log.d(DEBUG_TAG, responseFromTheServer);
-                httpURLConnection.disconnect(); //disconnect from the server
-            }
-            //==================This portion will need to be edited if the app is being updated to take attendance for all the classes==================
-            if (responseFromTheServer.compareToIgnoreCase("ALREADY_PRESENT") == 0) { //this means today's attendance has already been taken
-                toastString = "Attendance for today has already been taken";
-            }
-        } catch (Exception e) {
-            Log.d(DEBUG_TAG, e.toString());
-        }
+    }
+
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        //saves activity data for resuming when the activity is redrawn (for example after an accidental or intentional orientation change)
+        outState.putSerializable("studentAttendanceArray", studentAttendanceArray);
+        outState.putSerializable("jsonIndex", jsonIndex);
     }
 }
 
